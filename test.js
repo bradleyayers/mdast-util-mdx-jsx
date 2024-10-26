1594507c3753432b22731a0fc05ac1ed1606f455
Bradley Ayers <bradley.ayers@gmail.com>
2024-10-26 16:22:44 +1100

    Test cases for multiline string expressions

--- test.js
+++ test.js
@@ -2622,6 +2622,26 @@ test('roundtrip', async function (t) {
       equal(source, source)
     }
   )
+
+  await t.test(
+    'should roundtrip `JSX attributes containing new lines`',
+    async function () {
+      equal(
+        `<a\n  text={\`\n0\n 1\n  2\n   3\n    4\n     5\`}\n/>\n`,
+        `<a\n  text={\`\n0\n 1\n  2\n   3\n    4\n     5\`}\n/>\n`
+      )
+    }
+  )
+
+  await t.test(
+    'should roundtrip `JSX multiline string children`',
+    async function () {
+      equal(
+        `<a\n  trigger={<b>{\`\n0\n 1\n  2\n   3\n    4\n     5\`}</b>}\n/>\n`,
+        `<a\n  trigger={<b>{\`\n0\n 1\n  2\n   3\n    4\n     5\`}</b>}\n/>\n`
+      )
+    }
+  )
 })
 
 /**
